9025fb3ff551e3e16ee6f169ac2439cdd3641d45
mfranl <mflandry09@gmail.com>
2021-01-26 17:46:44 -0700

    created 3 questions in an object in an array in js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,18 @@
-js
+var timeEl = document.querySelector(".time");
+
+var secondsLeft = 60;
+
+function setTime() {
+    var timerInterval = setInterval(function() {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft;
+
+        if(secondsLeft === 0) {
+            clearInterval(timerInterval);
+            
+        }
+    }, 1000);
+}
+
+
+setTime();
